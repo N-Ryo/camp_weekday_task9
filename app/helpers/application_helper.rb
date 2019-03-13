cb03ffe82c07ce9bceb44cad321284f55ccae1fb
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-13 17:45:19 +0900

    Modify helper method name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def status_color(status)
-    return "#FF0000" if status.draft?
-    return "#0000FF" if status.published?
-    return "#008000" if status.archieved?
+  def status_color(task)
+    return "#FF0000" if task.draft?
+    return "#0000FF" if task.published?
+    return "#008000" if task.archieved?
   end
 end
